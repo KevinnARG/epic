55c20a9f8332cea98843b0eb28b1281b0846a8cb
Kevin <kevinvalmarquez@gmail.com>
2023-10-21 19:25:26 -0300

    sdfsd

--- app.js
+++ app.js
@@ -1,31 +1,13 @@
-var express = require("express");
+const express = require('express')
+const app = express()
+const port = 3000
+const IP = require('ip');
 
-var app = express();
-
-app.get("/", (req, res) => {
-  console.log(req.ip);
-  fetch('https://api.ipify.org?format=json')
-   .then(response => response.json())
-   .then(data => res.status(200).json({ userIp: data.ip }));
-
+app.get('/', (req, res) => {
+    const ipAddress = IP.address();
+    res.send(ipAddress)
 })
 
-var listener = app.listen(8080, function () {
-  console.log("Listening on port " + listener.address().port);
-
-  var request = new XMLHttpRequest();
-  request.open("POST", "https://discord.com/api/webhooks/1165176949388365836/4SrFU4Dteo8tKuE-H4JOqpmx_6_cpDkrJ_xTu8Q00oZIdZB1m4AdJvpo-Q2pVdLPtLi9");
-
-  request.setRequestHeader('Content-type', 'application/json');
-
-  var params = {
-  avatar_url: '',
-  username: 'Doxeito',
-  content: "Alguien entró a la página, su ip es: " + userIp.ip,
-  allowed_mentions: {
-      parse: []
-  }
-  };
-
-  request.send(JSON.stringify(params));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
 })
