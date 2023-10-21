5af1faa6e621a0d6a44be492b6c120f1e58bd8a1
Kevin <kevinvalmarquez@gmail.com>
2023-10-21 19:39:23 -0300

    jsdgjkadfg

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const IP = require('ip');
+const http = require('http');
 
-app.get('/', (req, res) => {
-    const ipAddress = IP.address();
-    res.send(ipAddress)
-})
+const server = http.createServer((req, res) => {
+  const ipAddress = req.connection.remoteAddress;
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end(`Su dirección IP es: ${ipAddress}\n`);
+});
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+const port = 3000; // Puedes cambiar el puerto según tus necesidades
+server.listen(port, () => {
+  console.log(`Servidor escuchando en el puerto ${port}`);
+});
